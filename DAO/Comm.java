ef7df1bb0f88a7ce87a555482330ad9ef0f4f77f
big-choi <80212139+big-choi@users.noreply.github.com>
2021-07-25 15:33:07 +0900

    Create Comm.java

---
+++ DAO/Comm.java
@@ -0,0 +1,52 @@
+package comm;
+
+public class Comm {
+	private int commID;
+	private String commTitle;
+	private String userID;
+	private String commDate;
+	private String commContent;
+	private int commAvailable;
+	
+	
+	public int getCommID() {
+		return commID;
+	}
+	public void setCommID(int commID) {
+		this.commID = commID;
+	}
+	public String getCommTitle() {
+		return commTitle;
+	}
+	public void setCommTitle(String commTitle) {
+		this.commTitle = commTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getCommDate() {
+		return commDate;
+	}
+	public void setCommDate(String commDate) {
+		this.commDate = commDate;
+	}
+	public String getCommContent() {
+		return commContent;
+	}
+	public void setCommContent(String commContent) {
+		this.commContent = commContent;
+	}
+	public int getCommAvailable() {
+		return commAvailable;
+	}
+	public void setCommAvailable(int commAvailable) {
+		this.commAvailable = commAvailable;
+	}
+	
+	
+	
+}
+
